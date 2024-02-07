0a9833d4bcb33466a489d3fda0adacea44baaf83
132435ty <Kz89267908477@gmail.com>
2024-02-07 20:46:19 +0300

    123

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 
-def filter_words(word_list):
-    filtered_list = [word for word in word_list if len(word) >= 4]
-    sorted_list = sorted(filtered_list)
-    return sorted_list
-
 words = ["яблоко", "стол", "книга", "дом", "кот", "автомобиль", "компьютер"]
-filtered_words = filter_words(words)
-print(filtered_words)
+z = list(sorted(words, key=lambda q: len(q)))
+print(z)
